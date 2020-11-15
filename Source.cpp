ebe11fa654926654b489300239628f4e24bf0447
наталя ДУЖАК <Duzhak.N@gmail.com>
2020-11-16 02:00:43 +0300

    6.1

--- Source.cpp
+++ Source.cpp
@@ -2,25 +2,26 @@
 #include <iomanip>
 #include <time.h>
 using namespace std;
-void Create(int* a, const int size, const int Low, const int High)
+void Create(int* a, const int n, const int Low, const int High)
 {
 	for (int i = 0; i <= 21; i++)
 		a[i] = Low + rand() % (High - Low + 1);
 	
 }
 
-void Print(int* a, const int size)
+void Print(int* a, const int n)
 {
 	for (int i = 0; i <= 21; i++)
 		cout << setw(4) << a[i];
 	cout << endl;
 }
 
-int Sum(int* a, const int size)
+int Sum(int* a, const int n)
 {
 	int S = 0;
-	for (int i = 0; i<=21;i++)
-		if( a[i] > 0 && (a[i] % 3) != 0)
+	for (int i = 0; i <= 21;i++)
+
+		if (a[i] > 0 || a[i] % 3 != 0)
 			S += a[i];
 	return S;
 }
